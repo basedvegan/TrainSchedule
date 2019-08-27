684861ff7fdd455c60ed07c59fbbba2360b83248
basedvegan <pojomanda@gmail.com>
2019-08-27 15:26:48 -0700

    firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC2UxCN17aIqzv75WnXFDcA3iLqCcSsKXQ",
+    authDomain: "trainschedulerrr-d98f5.firebaseapp.com",
+    databaseURL: "https://trainschedulerrr-d98f5.firebaseio.com",
+    projectId: "trainschedulerrr",
+    storageBucket: "",
+    messagingSenderId: "546561228544",
+    appId: "1:546561228544:web:ee86d3650d7c8c50"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
